997085f2d0f260a9b80527e0d42d2b5c05eb517e
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-08-16 16:12:14 +0530

    Time: 50 ms (19.69%), Space: 84.7 MB (39.23%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -6,8 +6,11 @@
 var containsNearbyDuplicate = function(nums, k) {
     let duplicateMap = new Map();
     for(let i=0;i<nums.length;i++){
-        if(duplicateMap.has(nums[i]) && Math.abs(duplicateMap.get(nums[i])-i)<=k) return true;
-        duplicateMap.set(nums[i],i)
+        let num = nums[i]
+        if(duplicateMap.has(num)){
+            if(Math.abs(duplicateMap.get(num)-i)<=k) return true;
+        }    
+        duplicateMap.set(num,i)
     }
     return false;
 };
