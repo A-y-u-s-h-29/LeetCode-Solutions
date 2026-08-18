50cc20c88d6bc94f9909da5d11a4aeb2824f93f4
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-08-18 19:37:48 +0530

    Time: 38 ms (34.03%), Space: 60.1 MB (46.77%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -4,13 +4,12 @@
  */
 var firstUniqChar = function(s) {
     let firstMap = new Map();
-    for(let char of s){
-        firstMap.set(char,(firstMap.get(char) ||0)+1);
+    for(let i=0;i<s.length;i++){
+        firstMap.set(s.charAt(i),(firstMap.get(s.charAt(i)) ||0)+1);
     }
     for(let i=0;i<s.length;i++){
-        if(firstMap.get(s.charAt(i))===1){
-            return i
-        }
+        if(firstMap.get(s.charAt(i))===1) return i
     }
+   
     return -1;
 };
