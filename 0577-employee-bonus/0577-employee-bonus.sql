9990d1109273ad56d05f2fbb777c8c177709b30f
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-08-25 12:39:15 +0530

    Time: 1046 ms (63.24%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT e.name,b.bonus
-FROM Employee e
+SELECT e.name,b.bonus FROM Employee e
 LEFT JOIN Bonus b
     ON e.empId = b.empId
 WHERE b.bonus IS NULL
